f8a7ce910ac2c42c03805e9d93fa868af5c91ee4
Aaron Know <aaronhdz1919@gmail.com>
2022-06-23 22:43:03 -0600

    Terminado, pero falta probar

--- src/Percolation.java
+++ src/Percolation.java
@@ -1,10 +1,8 @@
-import java.sql.Struct;
-
 import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 
 public class Percolation
 {  
-    private Nodo[][] grid;
+    private Nodo grid[][];
     private int openSites;
     private boolean percolate;
     private int rowsNCol;
@@ -22,18 +20,21 @@ public class Percolation
 
         if (n==1) 
         {
-                grid[0][0].name=0;
-                grid[0][0].open=false;
-                grid[0][0].full=false;
-                grid[0][0].top=true;
-                grid[0][0].bottom=true;
-                return;            
+            grid[0][0]=new Nodo();
+            grid[0][0].name=0;
+            grid[0][0].open=false;
+            grid[0][0].full=false;
+            grid[0][0].top=true;
+            grid[0][0].bottom=true;
+            return;            
         }
         
         int counter = 0;
 
+       
         for(int j=0; j<n; j++) //This is only for the first row of the matrix.
-        {            
+        {   
+            grid[0][j]=new Nodo();         
             grid[0][j].name=counter;
             grid[0][j].open=false;
             grid[0][j].full=false;
@@ -44,8 +45,9 @@ public class Percolation
 
         for (int i = 1; i < n-1; i++) // This is for the body of the matrix.
         {
-            for (int j = 1; j < n; j++)
+            for (int j = 0; j < n; j++)
             {
+                grid[i][j]=new Nodo();
                 grid[i][j].name=counter;
                 grid[i][j].open=false;
                 grid[i][j].full=false;
@@ -57,41 +59,47 @@ public class Percolation
         }
         for (int j = 0; j < n; j++)
         {
-            grid[0][j].name=counter;
-            grid[0][j].open=false;
-            grid[0][j].full=false;
-            grid[0][j].top=false;
-            grid[0][j].bottom=true;
+            grid[n-1][j]=new Nodo();
+            grid[n-1][j].name=counter;
+            grid[n-1][j].open=false;
+            grid[n-1][j].full=false;
+            grid[n-1][j].top=false;
+            grid[n-1][j].bottom=true;
             counter++;
         }
-        union = new WeightedQuickUnionUF(n*n+1);// Its because it's necessary to have one more space
-                                                // to know when the system percolates, representing the top of the system.
-        
+        union = new WeightedQuickUnionUF(n*n);
     }   
     // // opens the site (row, col) if it is not open already
     public void open(int row, int col)
     {
-       checkRowsAndCol(row, col);
-       
-        //TODO: cuando se abra un nodo hay que unirlo a algun conjunto si es que es posible.
-       
+       checkRowsAndCol(row, col);                
        row-=1;
        col-=1;
 
-       if(isOpen(row, col))
+       if(isOpen(row, col)) 
             return;
         
+        // Open and union this node.    
         if(grid[row][col].top==true)
         {
             grid[row][col].open=true;
             grid[row][col].full=true;
             connectNeighbor(row,col);
-            // TODO: al nodo raiz cambiarle el valor de full.
-            union.find(grid[row][col].name);
-        }
-                   
-            grid[row][col].open = true;
+            Nodo root = findNodo(union.find(grid[row][col].name));
+            root.full=true;
             openSites++;
+            return;
+        }
+
+        grid[row][col].open=true;
+        connectNeighbor(row,col);
+        Nodo root = findNodo(union.find(grid[row][col].name));
+        if(root.full==true)
+            grid[row][col].full=true;
+        openSites++;
+        if(grid[row][col].full==true && grid[row][col].bottom==true)
+            percolate=true;
+        return;
        
     }
     
@@ -107,7 +115,26 @@ public class Percolation
     }
     
     // // is the site (row, col) full?
-    // public boolean isFull(int row, int col)
+    public boolean isFull(int row, int col)
+    {
+        checkRowsAndCol(row, col);
+        if(!isOpen(row, col)) // If the node its close.
+            return false;
+
+        if(grid[row][col].full==true)
+            return true;
+        else
+        {
+            Nodo root = findNodo(union.find(grid[row][col].name));
+            if(root.full==true)
+            {
+                grid[row][col].full=true;
+                percolate=(grid[row][col].bottom==true)?true:false;
+                return true;
+            }
+            return false;
+        }
+    }
     
     // returns the number of open sites
     public int numberOfOpenSites()
@@ -116,10 +143,27 @@ public class Percolation
     }
     
     // // does the system percolate?
-    // public boolean percolates()
+    public boolean percolates()
+    {
+        if(percolate==true)
+            return true;
+        else
+        {
+            for (int j = 0; j < rowsNCol; j++)
+            {
+              if(isFull(rowsNCol-1, j))
+                return true;
+            }
+            return false;
+        }
+    }
     
     // // // test client (optional)
     // // public static void main(String[] args)
+
+    ////////////////////////////////////////////////////////////////////////
+    //                      PRIVATE FUNCIONS                             //
+    ////////////////////////////////////////////////////////////////////// 
 
     private void checkRowsAndCol(int row, int col)
     {
@@ -144,6 +188,13 @@ public class Percolation
         if((col+1<=rowsNCol) && (isOpen(row, col+1)))
             union.union(grid[row][col].name, grid[row][col-1].name);
         
+    }
+    private Nodo findNodo(int clave)
+    {
+        int fila = clave/rowsNCol;
+        int columna = clave-(fila*rowsNCol);
+        return grid[fila][columna];
+
     }
     
     private class Nodo
